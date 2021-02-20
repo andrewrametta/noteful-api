16aa33ea52f1cac2dab94b764068e7b42216d0bc
Andrew <ramettaandrewj@gmail.com>
2021-02-20 17:08:56 -0500

    added folder router with get and post

---
+++ src/folders/folders-router.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const xss = require("xss");
+const FoldersService = require("./folders-service");
+
+const folderRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeFolder = (folder) => ({
+  id: folder.id,
+  name: folder.name,
+});
+
+folderRouter
+  .route("/")
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    FoldersService.getAllFolders(knexInstance)
+      .then((folders) => {
+        res.json(folders.map(serializeFolder));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name } = req.body;
+    const newFolder = { name };
+
+    for (const [key, value] of Object.entries(newFolder))
+      if (value === null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+    FoldersService.insertFolder(knexInstance, newFolder)
+      .then((folder) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializeFolder(folder));
+      })
+      .catch(next);
+  });
