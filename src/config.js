8da2717ba150f99558751ddad7be40a638c2ab62
Andrew <ramettaandrewj@gmail.com>
2021-02-23 16:51:11 -0500

    getting ready for heroku deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://Metta@localhost/noteful_api",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://Metta@localhost/noteful_api",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://Metta@localhost/noteful_api_test",
 };
