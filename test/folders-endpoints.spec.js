65b103fc74b0219cb2cf103b1c93713ac83c1957
Andrew <ramettaandrewj@gmail.com>
2021-02-23 10:13:34 -0500

    added test for GET folder id

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -39,4 +39,31 @@ describe("Folders Endpoints", function () {
       });
     });
   });
+
+  describe(`GET /api/folders/:folder_id`, () => {
+    context(`Given no folders`, () => {
+      it(`responds with 404`, () => {
+        const folderId = 123456;
+        return supertest(app)
+          .get(`/api/folders/${folderId}`)
+          .expect(404, { error: { message: `Folder does not exist` } });
+      });
+    });
+
+    context("Given there are folders in the database", () => {
+      const testFolders = makeFoldersArray();
+
+      beforeEach("insert folders", () => {
+        return db.into("folders").insert(testFolders);
+      });
+
+      it("responds with 200 and the specified folder", () => {
+        const folderId = 2;
+        const expectedFolder = testFolders[folderId - 1];
+        return supertest(app)
+          .get(`/api/folders/${folderId}`)
+          .expect(200, expectedFolder);
+      });
+    });
+  });
 });
